a9024f90d3c9098719eedc3d73cff14f16393e3f
xianchaoxiu <128447891+xianchaoxiu@users.noreply.github.com>
2025-03-11 14:24:01 +0800

    Delete proximal_operator directory

--- proximal_operator/twist2.m
+++
@@ -1,8 +0,0 @@
-function B = twist2(A)
-%   horizontal slices correspond to channels
-[n1,n2,n3] = size(A);
-B = zeros(n3,n2,n1);
-for i = 1 : n3
-   slice = A(:,:,i);
-   B(i,:,:) = reshape(slice,1,n2,n1);    
-end
